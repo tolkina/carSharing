86a0dea1b7e39770b541eb0aa0299d4d31665509
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-10-12 11:53:56 +0300

    Initial commit

---
+++ src/main/java/com/exposit/carsharing/CarSharingApplication.java
@@ -0,0 +1,12 @@
+package com.exposit.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSharingApplication.class, args);
+	}
+}
