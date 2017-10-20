d54861298aa0dbc1a73bb9a6eb6c1a5526516e40
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-10-20 19:12:36 +0300

    Added functional in all technical parameters, but need to improve brand and model

--- src/webApp/car-sharing/src/app/technical-parameters/fuel-type/fuel-type.component.ts
+++ src/webApp/car-sharing/src/app/technical-parameters/fuel-type/fuel-type.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TechnicalParameter} from "../../model/technical-parameter";
+import {TechnicalParameterService} from "../../service/technical-parameter.service";
 
 @Component({
   selector: 'app-fuel-type',
@@ -6,10 +8,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fuel-type.component.css']
 })
 export class FuelTypeComponent implements OnInit {
+  name = "fuelType";
+  parameters: TechnicalParameter[];
+  newParameter: any = {};
+  editedParameter: any = {};
+  error: String = "";
+  cloneParameter: any = {};
+  flag: boolean = false;
 
-  constructor() { }
+  constructor(private technicalParameterService: TechnicalParameterService) {
+  }
 
   ngOnInit() {
+    this.getTechnicalParameters();
+  }
+
+  getTechnicalParameters() {
+    this.technicalParameterService.getParameters(this.name)
+      .then(parameters => {
+          this.parameters = parameters;
+          this.clearError();
+        }
+      )
+      .catch();
+  }
+
+  showEditParameterModal(parameter: TechnicalParameter) {
+    this.cloneParameter = parameter;
+    this.editedParameter.id = parameter.id;
+    this.flag = true;
+  }
+
+  showDeleteParameterModal(parameter: TechnicalParameter) {
+    this.cloneParameter = parameter;
+  }
+
+  saveParameter(parameter: TechnicalParameter) {
+    this.technicalParameterService.addParameter(this.name, parameter)
+      .then(result => {
+        this.parameters.push(result);
+        this.cancelNewParameter();
+        this.clearError();
+      })
+      .catch();
+  }
+
+  removeParameter(parameter: TechnicalParameter) {
+    this.technicalParameterService.deleteParameter(this.name, parameter)
+      .then(result => {
+        this.parameters.splice(this.parameters.indexOf(parameter), 1);
+        this.clearError();
+      }).catch();
+  }
+
+  updateParameter() {
+    this.technicalParameterService.updateParameter(this.name, this.editedParameter)
+      .then(result => {
+        this.parameters[this.parameters.indexOf(this.cloneParameter)].name = result.name;
+        this.cancelEdits();
+        this.clearError();
+      }).catch();
+  }
+
+  cancelNewParameter() {
+    this.newParameter = {};
+  }
+
+  cancelEdits() {
+    this.editedParameter = {};
+    this.cloneParameter = {};
+  }
+
+  clearError() {
+    this.error = "";
   }
 
 }
