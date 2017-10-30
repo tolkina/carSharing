ee91243a58bf0178f6d0076303417925771a6b56
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-10-30 15:06:16 +0300

    Transfered admin-panel

--- src/webApp/car-sharing/src/app/app-routing/app-routing.module.ts
+++ src/webApp/car-sharing/src/app/app-routing/app-routing.module.ts
@@ -1,42 +1,52 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 
-import {ProfileComponent} from "../profile/profile.component";
-import {ProfileInfoComponent} from "../profile-info/profile-info.component";
-import {ProfileCarComponent} from "../profile-car/profile-car.component";
-import {ProfileAdComponent} from "../profile-ad/profile-ad.component";
-import {AllCarsComponent} from "../all-cars/all-cars.component"
-import {NewCarComponent} from "../new-car/new-car.component"
-import {CurrentCarComponent} from "../current-car/current-car.component"
-import {GeneralParametersComponent} from "../general-parameters/general-parameters.component";
-import {TechnicalParametersComponent} from "../technical-parameters/technical-parameters.component";
-import {CurrentConditionComponent} from "../current-condition/current-condition.component";
+import {ProfileComponent} from "../user/profile/profile.component";
+import {ProfileInfoComponent} from "../user/profile-info/profile-info.component";
+import {ProfileCarComponent} from "../user/profile-car/profile-car.component";
+import {ProfileAdComponent} from "../user/profile-ad/profile-ad.component";
+import {AllCarsComponent} from "../user/all-cars/all-cars.component"
+import {NewCarComponent} from "../user/new-car/new-car.component"
+import {CurrentCarComponent} from "../user/current-car/current-car.component"
+import {GeneralParametersComponent} from "../user/general-parameters/general-parameters.component";
+import {TechnicalParametersComponent} from "../user/technical-parameters/technical-parameters.component";
+import {CurrentConditionComponent} from "../user/current-condition/current-condition.component";
+import {UserHomePageComponent} from "../user/user-home-page/user-home-page.component";
+import {HomePageAdminComponent} from "../admin/home-page-admin/home-page-admin.component";
+import {SetupAdminComponent} from "../admin/setup-admin/setup-admin.component";
 
 const routes: Routes = [
   {
-    path: 'profile', component: ProfileComponent,
-    children: [
-      {path: '', redirectTo: 'info', pathMatch: 'full'},
-      {path: 'info', component: ProfileInfoComponent},
-      {
-        path: 'car', component: ProfileCarComponent,
-        children: [
-          {path: '', redirectTo: 'all', pathMatch: 'full'},
-          {path: 'all', component: AllCarsComponent},
-          {path: 'new', component: NewCarComponent},
-          {
-            path: ':carId', component: CurrentCarComponent,
-            children: [
-              {path: '', redirectTo: 'general-parameters', pathMatch: 'full'},
-              {path: 'general-parameters', component: GeneralParametersComponent},
-              {path: 'technical-parameters', component: TechnicalParametersComponent},
-              {path: 'current-condition', component: CurrentConditionComponent}
-            ]
-          },
-        ]
-      },
-      {path: 'ad', component: ProfileAdComponent}
-    ]
+    path: 'admin', component: HomePageAdminComponent, children: [
+    {path: 'setup', component: SetupAdminComponent}]
+  },
+  {
+    path: '', component: UserHomePageComponent, children: [
+    {
+      path: 'profile', component: ProfileComponent,
+      children: [
+        {path: '', redirectTo: 'info', pathMatch: 'full'},
+        {path: 'info', component: ProfileInfoComponent},
+        {
+          path: 'car', component: ProfileCarComponent,
+          children: [
+            {path: '', redirectTo: 'all', pathMatch: 'full'},
+            {path: 'all', component: AllCarsComponent},
+            {path: 'new', component: NewCarComponent},
+            {
+              path: ':carId', component: CurrentCarComponent,
+              children: [
+                {path: '', redirectTo: 'general-parameters', pathMatch: 'full'},
+                {path: 'general-parameters', component: GeneralParametersComponent},
+                {path: 'technical-parameters', component: TechnicalParametersComponent},
+                {path: 'current-condition', component: CurrentConditionComponent}
+              ]
+            },
+          ]
+        },
+        {path: 'ad', component: ProfileAdComponent}
+      ]
+    }]
   }
 ];
 
--- src/webApp/car-sharing/src/app/app.module.ts
+++ src/webApp/car-sharing/src/app/app.module.ts
@@ -4,22 +4,35 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 
 import {AppComponent} from './app.component';
-import {ProfileService} from "./service/profile.service";
+import {ProfileService} from "./user/service/profile.service";
 import {AppRoutingModule} from "./app-routing/app-routing.module";
-import {ProfileComponent} from "./profile/profile.component";
-import {PassportDataService} from "./service/passport-data.service";
-import {DriverLicenseService} from "./service/driver-license.service";
-import {ProfileInfoComponent} from './profile-info/profile-info.component';
-import {ProfileCarComponent} from './profile-car/profile-car.component';
-import {ProfileAdComponent} from './profile-ad/profile-ad.component';
-import {ProfileCarService} from "./service/profile-car.service";
-import {AllCarsComponent} from './all-cars/all-cars.component';
-import {NewCarComponent} from './new-car/new-car.component';
-import {CurrentCarComponent} from './current-car/current-car.component';
-import {TechnicalParametersComponent} from './technical-parameters/technical-parameters.component';
-import {GeneralParametersComponent} from './general-parameters/general-parameters.component';
-import {CurrentConditionComponent} from './current-condition/current-condition.component';
-import {CarParameterService} from "./service/car-parameter.service";
+import {ProfileComponent} from "./user/profile/profile.component";
+import {PassportDataService} from "./user/service/passport-data.service";
+import {DriverLicenseService} from "./user/service/driver-license.service";
+import {ProfileInfoComponent} from './user/profile-info/profile-info.component';
+import {ProfileCarComponent} from './user/profile-car/profile-car.component';
+import {ProfileAdComponent} from './user/profile-ad/profile-ad.component';
+import {ProfileCarService} from "./user/service/profile-car.service";
+import {AllCarsComponent} from './user/all-cars/all-cars.component';
+import {NewCarComponent} from './user/new-car/new-car.component';
+import {CurrentCarComponent} from './user/current-car/current-car.component';
+import {TechnicalParametersComponent} from './user/technical-parameters/technical-parameters.component';
+import {GeneralParametersComponent} from './user/general-parameters/general-parameters.component';
+import {CurrentConditionComponent} from './user/current-condition/current-condition.component';
+import {CarParameterService} from "./user/service/car-parameter.service";
+import { UserHomePageComponent } from './user/user-home-page/user-home-page.component';
+import {TechnicalParameterService} from "./admin/service/technical-parameter.service";
+import {HomePageAdminComponent} from "./admin/home-page-admin/home-page-admin.component";
+import {SetupAdminComponent} from "./admin/setup-admin/setup-admin.component";
+import {GearboxComponent} from "./admin/technical-parameters/gearbox/gearbox.component";
+import {BrandComponent} from "./admin/technical-parameters/brand/brand.component";
+import {ModelComponent} from "./admin/technical-parameters/model/model.component";
+import {ColorComponent} from "./admin/technical-parameters/color/color.component";
+import {DriveUnitComponent} from "./admin/technical-parameters/drive-unit/drive-unit.component";
+import {FuelTypeComponent} from "./admin/technical-parameters/fuel-type/fuel-type.component";
+import {InteriorMaterialComponent} from "./admin/technical-parameters/interior-material/interior-material.component";
+import {TiresSeasonComponent} from "./admin/technical-parameters/tires-season/tires-season.component";
+import {BodyTypeComponent} from "./admin/technical-parameters/body-type/body-type.component";
 
 @NgModule({
   declarations: [
@@ -33,7 +46,19 @@ import {CarParameterService} from "./service/car-parameter.service";
     CurrentCarComponent,
     TechnicalParametersComponent,
     GeneralParametersComponent,
-    CurrentConditionComponent
+    CurrentConditionComponent,
+    UserHomePageComponent,
+    HomePageAdminComponent,
+    SetupAdminComponent,
+    GearboxComponent,
+    BrandComponent,
+    ModelComponent,
+    ColorComponent,
+    DriveUnitComponent,
+    FuelTypeComponent,
+    InteriorMaterialComponent,
+    TiresSeasonComponent,
+    BodyTypeComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +66,8 @@ import {CarParameterService} from "./service/car-parameter.service";
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ProfileService, PassportDataService, DriverLicenseService, ProfileCarService, CarParameterService],
+  providers: [ProfileService, PassportDataService, DriverLicenseService, ProfileCarService,
+    CarParameterService, TechnicalParameterService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
