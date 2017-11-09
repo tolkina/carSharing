56fb91efdd4fb60bb2eec634a92616ccb3ce07c6
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-11-09 20:40:18 +0300

    Сhanged modal, added valid for currentCondition

--- src/web/car-sharing/src/app/user/current-condition/current-condition.component.ts
+++ src/web/car-sharing/src/app/user/current-condition/current-condition.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProfileCarService} from "../service/profile-car.service";
 import {clone} from "lodash";
 import {ActivatedRoute, Router} from '@angular/router'
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-current-condition',
@@ -12,13 +13,25 @@ export class CurrentConditionComponent implements OnInit {
   carId: number;
   currentCondition: any = {};
   editedCurrentCondition: any = {};
+  modalRef: any;
+  errorUpdate: String;
 
   constructor(private carService: ProfileCarService, private activateRoute: ActivatedRoute,
-              private router: Router) {
+              private router: Router, private modalService: NgbModal) {
     this.carId = +activateRoute.snapshot.parent.params['carId'];
     if (isNaN(this.carId)) {
       this.router.navigateByUrl("profile/car")
     }
+  }
+
+  onSubmit() {
+    this.updateCurrentCondition()
+  }
+
+  showEdit(content) {
+    this.errorUpdate = "";
+    this.modalRef = this.modalService.open(content);
+    this.editedCurrentCondition = clone(this.currentCondition);
   }
 
   ngOnInit() {
@@ -27,17 +40,16 @@ export class CurrentConditionComponent implements OnInit {
 
   updateCurrentCondition() {
     this.carService.updateCurrentCondition(this.editedCurrentCondition, this.carId).then()
-      .then(res => this.currentCondition = res)
-      .catch();
+      .then(res => {
+        this.currentCondition = res;
+        this.modalRef.close()
+      })
+      .catch(err => this.errorUpdate = err._body);
   }
 
   getCurrentCondition(carId: number) {
     this.carService.getCurrentCondition(carId).then()
       .then(res => this.currentCondition = res)
       .catch(res => this.router.navigateByUrl("profile/car"));
-  }
-
-  editParams() {
-    this.editedCurrentCondition = clone(this.currentCondition);
   }
 }
