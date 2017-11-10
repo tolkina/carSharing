0b0dc57cc78b3c6ba5534b9451023f1f4110884a
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-11-10 13:04:10 +0300

    Added valid, changed modal for brand.

--- src/web/car-sharing/src/app/admin/technical-parameters/brand/brand.component.ts
+++ src/web/car-sharing/src/app/admin/technical-parameters/brand/brand.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TechnicalParameter} from "../../domain/technical-parameter";
 import {TechnicalParameterService} from "../../service/technical-parameter.service";
 import {Brand_} from "../../domain/brand_";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-brand',
@@ -11,17 +12,22 @@ import {Brand_} from "../../domain/brand_";
 export class BrandComponent implements OnInit {
   name = "brand";
   parameters: Brand_[];
-  newParameter: any = {};
-  newModel: any = {};
-  editedParameter: any = {};
-  editedModel: any = {};
-  error: String = "";
-  cloneParameter: any = {};
-  cloneModel: any = {};
+  newBrand = new Brand_();
+  newModel = new TechnicalParameter();
+  editedBrand = new Brand_();
+  editedModel = new TechnicalParameter();
+  cloneBrand = new Brand_();
+  cloneModel = new TechnicalParameter();
+  errorDelete: String;
+  errorCreate: String;
+  errorUpdate: String;
+  createModalRef: any;
+  updateModalRef: any;
+  deleteModalRef: any;
   flag: boolean = false;
   flag_model: number;
 
-  constructor(private technicalParameterService: TechnicalParameterService) {
+  constructor(private technicalParameterService: TechnicalParameterService, private modalService: NgbModal) {
   }
 
   ngOnInit() {
@@ -37,58 +43,6 @@ export class BrandComponent implements OnInit {
       .catch();
   }
 
-  showEditParameterModal(parameter: Brand_) {
-    this.cloneParameter = parameter;
-    this.editedParameter = {};
-    this.editedParameter.id = parameter.id;
-    this.flag = true;
-  }
-
-  showDeleteParameterModal(parameter: Brand_) {
-    this.cloneParameter = parameter;
-  }
-
-  saveParameter(parameter: Brand_) {
-    this.technicalParameterService.addParameter(this.name, parameter)
-      .then(result => {
-        result.models = [];
-        this.parameters.push(result);
-        this.cancelNewParameter();
-        this.clearError();
-      })
-      .catch();
-  }
-
-  removeParameter(parameter: Brand_) {
-    this.technicalParameterService.deleteParameter(this.name, parameter)
-      .then(result => {
-        this.parameters.splice(this.parameters.indexOf(parameter), 1);
-        this.clearError();
-      }).catch();
-  }
-
-  updateParameter() {
-    this.technicalParameterService.updateParameter(this.name, this.editedParameter)
-      .then(result => {
-        this.parameters[this.parameters.indexOf(this.cloneParameter)].name = result.name;
-        this.cancelEdits();
-        this.clearError();
-      }).catch();
-  }
-
-  cancelNewParameter() {
-    this.newParameter = {};
-  }
-
-  cancelEdits() {
-    this.editedParameter = {};
-    this.cloneParameter = {};
-  }
-
-  clearError() {
-    this.error = "";
-  }
-
   showModels(parameter) {
     if (this.flag_model == parameter.id) {
       this.flag_model = null
@@ -98,45 +52,108 @@ export class BrandComponent implements OnInit {
     }
   }
 
-  saveModel() {
-    this.technicalParameterService.addModel(this.cloneParameter, this.newModel)
+  saveBrand() {
+    this.technicalParameterService.addParameter(this.name, this.newBrand)
       .then(result => {
-        if (this.cloneParameter.models == null) {
-          this.cloneParameter.models = [];
-        }
-        this.cloneParameter.models.push(result)
+        result.models = [];
+        this.parameters.push(result);
+        this.createModalRef.close();
       })
-      .catch();
+      .catch(err => this.errorCreate = err);
+  }
+
+  saveModel() {
+    this.technicalParameterService.addModel(this.cloneBrand, this.newModel)
+      .then(result => {
+        if (this.cloneBrand.models == null) {
+          this.cloneBrand.models = [];
+        }
+        this.cloneBrand.models.push(result);
+        this.createModalRef.close();
+      })
+      .catch(err => this.errorCreate = err);
+  }
+
+  removeBrand() {
+    this.technicalParameterService.deleteParameter(this.name, this.cloneBrand)
+      .then(result => {
+        this.parameters.splice(this.parameters.indexOf(this.cloneBrand), 1);
+        this.deleteModalRef.close();
+      })
+      .catch(err => this.errorDelete = err);
   }
 
   removeModel() {
     this.technicalParameterService.deleteParameter("model", this.cloneModel)
       .then(result => {
-        this.cloneParameter.models.splice(this.cloneParameter.models.indexOf(this.cloneModel), 1);
-      }).catch();
+        this.cloneBrand.models.splice(this.cloneBrand.models.indexOf(this.cloneModel), 1);
+        this.deleteModalRef.close();
+      })
+      .catch(err => this.errorDelete = err);
+  }
+
+
+  updateBrand() {
+    this.technicalParameterService.updateParameter(this.name, this.editedBrand)
+      .then(result => {
+        this.parameters[this.parameters.indexOf(this.cloneBrand)].name = result.name;
+        this.updateModalRef.close();
+      })
+      .catch(err => this.errorUpdate = err);
   }
 
   updateModel() {
     this.technicalParameterService.updateParameter("model", this.editedModel)
       .then(result => {
         this.cloneModel.name = result.name;
-      }).catch();
+        this.updateModalRef.close();
+      })
+      .catch(err => this.errorUpdate = err);
   }
 
-  showEditModelModal(brand: Brand_, model: TechnicalParameter) {
-    this.cloneParameter = brand;
+
+  showCreateBrand(content) {
+    this.errorCreate = "";
+    this.createModalRef = this.modalService.open(content);
+    this.newBrand = new Brand_();
+  }
+
+  showDeleteBrand(parameter: Brand_, content) {
+    this.errorDelete = "";
+    this.deleteModalRef = this.modalService.open(content);
+    this.cloneBrand = parameter;
+  }
+
+  showUpdateBrand(parameter: Brand_, content) {
+    this.errorUpdate = "";
+    this.updateModalRef = this.modalService.open(content);
+    this.cloneBrand = parameter;
+    this.editedBrand = new Brand_();
+    this.editedBrand.id = parameter.id;
+    this.flag = true;
+  }
+
+  showCreateModel(brand: Brand_, content) {
+    this.errorCreate = "";
+    this.createModalRef = this.modalService.open(content);
+    this.cloneBrand = brand;
+    this.newModel = new TechnicalParameter();
+  }
+
+  showDeleteModel(brand: Brand_, model: TechnicalParameter, content) {
+    this.errorDelete = "";
+    this.deleteModalRef = this.modalService.open(content);
+    this.cloneBrand = brand;
     this.cloneModel = model;
-    this.editedModel = {};
+  }
+
+  showUpdateModel(brand: Brand_, model: TechnicalParameter, content) {
+    this.errorUpdate = "";
+    this.updateModalRef = this.modalService.open(content);
+    this.cloneBrand = brand;
+    this.cloneModel = model;
+    this.editedModel = new TechnicalParameter();
     this.editedModel.id = model.id;
   }
 
-  showDeleteModelModal(brand: Brand_, model: TechnicalParameter) {
-    this.cloneParameter = brand;
-    this.cloneModel = model;
-  }
-
-  showNewModelModal(brand: Brand_) {
-    this.cloneParameter = brand;
-    this.newModel = {};
-  }
 }
