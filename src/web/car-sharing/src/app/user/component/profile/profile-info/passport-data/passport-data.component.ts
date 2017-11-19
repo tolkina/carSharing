03a2c27901c7e6dcf27030925b258dceb9ab1d65
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-11-19 14:35:24 +0300

    Improved validation in passportData.

--- src/web/car-sharing/src/app/user/component/profile/profile-info/passport-data/passport-data.component.ts
+++ src/web/car-sharing/src/app/user/component/profile/profile-info/passport-data/passport-data.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {PassportData} from "../../../../domain/passport-data";
 import {PassportDataService} from "../../../../service/passport-data.service";
 import {clone} from "lodash";
-import {NgbDatepickerConfig, NgbDateStruct, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgbDateStruct, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {DateFormatter} from "../../../../../date-formatter";
 import {ProfileInfoComponent} from "../profile-info.component";
 
@@ -19,15 +19,15 @@ export class PassportDataComponent implements OnInit {
   editedPassport: PassportData = new PassportData();
   dateOfIssue: NgbDateStruct;
   validUntil: NgbDateStruct;
+  ngbValidUntil: NgbDateStruct[] = [];
+  ngbDateOfIssue: NgbDateStruct[] = [];
 
-  constructor(private passportService: PassportDataService, config: NgbDatepickerConfig,
-              private dateFormatter: DateFormatter, private modalService: NgbModal,
-              private profileInfoComponent: ProfileInfoComponent) {
-    config.minDate = {year: 1980, month: 1, day: 1};
-    config.maxDate = {year: 2099, month: 12, day: 31};
+  constructor(private passportService: PassportDataService, private dateFormatter: DateFormatter,
+              private modalService: NgbModal, private profileInfoComponent: ProfileInfoComponent) {
   }
 
   ngOnInit() {
+    this.putNgbBorders();
     this.getPassportData();
   }
 
@@ -40,14 +40,16 @@ export class PassportDataComponent implements OnInit {
   }
 
   updatePassport() {
-    this.passportService.updatePassport(this.editedPassport)
-      .then(passport => {
-        this.modalRef.close();
-        this.passport = passport;
-        this.editedPassport = new PassportData();
-        this.profileInfoComponent.profile.confirmProfile = this.profileInfoComponent.noConfirm;
-      })
-      .catch(err => this.errorUpdate = err._body);
+    if (this.checkNgbDates()) {
+      this.passportService.updatePassport(this.editedPassport)
+        .then(passport => {
+          this.modalRef.close();
+          this.passport = passport;
+          this.editedPassport = new PassportData();
+          this.profileInfoComponent.profile.confirmProfile = this.profileInfoComponent.noConfirm;
+        })
+        .catch(err => this.errorUpdate = err);
+    }
   }
 
   getPassportData() {
@@ -58,9 +60,33 @@ export class PassportDataComponent implements OnInit {
 
   showEdit(content) {
     this.errorUpdate = "";
-    this.modalRef = this.modalService.open(content);
+    this.modalRef = this.modalService.open(content, {size: 'lg'});
     this.editedPassport = clone(this.passport);
     this.dateOfIssue = this.dateFormatter.fromDate(this.passport.dateOfIssue);
     this.validUntil = this.dateFormatter.fromDate(this.passport.validUntil);
+    this.editedPassport.validUntil = this.dateFormatter.toDate(this.validUntil);
+    this.editedPassport.dateOfIssue = this.dateFormatter.toDate(this.dateOfIssue);
+  }
+
+  private putNgbBorders() {
+    const currentDate = new Date();
+    this.ngbDateOfIssue.push({year: 1980, month: 1, day: 1}, {
+      day: currentDate.getUTCDate(),
+      month: currentDate.getUTCMonth() + 1,
+      year: currentDate.getUTCFullYear()
+    });
+    this.ngbValidUntil.push({year: 1980, month: 1, day: 1}, {
+      day: currentDate.getUTCDate(),
+      month: currentDate.getUTCMonth() + 1,
+      year: currentDate.getUTCFullYear() + 20
+    });
+  }
+
+  private checkNgbDates() {
+    if (this.editedPassport.dateOfIssue > this.editedPassport.validUntil) {
+      this.errorUpdate = "Срок действия не может быть раньше даты выдачи";
+      return false;
+    }
+    return true;
   }
 }
