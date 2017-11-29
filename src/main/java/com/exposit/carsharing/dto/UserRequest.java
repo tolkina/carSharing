f8d2b5255ac7059004d0fbce6d43470c02791b56
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-11-29 13:03:12 +0300

    Added confirmPassword on backend

--- src/main/java/com/exposit/carsharing/dto/UserRequest.java
+++ src/main/java/com/exposit/carsharing/dto/UserRequest.java
@@ -1,6 +1,7 @@
 package com.exposit.carsharing.dto;
 
 import com.exposit.carsharing.util.Constants;
+import cz.jirutka.validator.spring.SpELAssert;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.NotBlank;
@@ -10,6 +11,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
+@SpELAssert(value = "password.equals(confirmPassword)", message = "Passwords are not the same")
 public class UserRequest implements Serializable {
     @Pattern(regexp = Constants.EMAIL_REGEX)
     @NotBlank
@@ -17,6 +19,9 @@ public class UserRequest implements Serializable {
     @Pattern(regexp = Constants.PASSWORD_REGEX)
     @NotBlank
     private String password;
+    @Pattern(regexp = Constants.PASSWORD_REGEX)
+    @NotBlank
+    private String confirmPassword;
     @NotBlank
     private String login;
 }
